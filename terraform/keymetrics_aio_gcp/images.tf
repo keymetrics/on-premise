b7379f3a67f030f5c30257f7777542d3154b519b
vmarchaud <thisismac47@gmail.com>
2018-03-20 14:08:15 +0100

    gcp: remove resolving of backend image

--- terraform/keymetrics_aio_gcp/images.tf
+++ terraform/keymetrics_aio_gcp/images.tf
@@ -1,8 +1,3 @@
-data "google_compute_image" "km-api" {
-  family = "km-api"
-  project = "dedicated-keymetrics"
-}
-
 data "google_compute_image" "ubuntu" {
   family = "ubuntu-1604-lts"
   project = "ubuntu-os-cloud"
